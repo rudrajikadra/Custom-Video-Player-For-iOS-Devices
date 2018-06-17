4f116e37addbed7fd6352072b515a4d7afad2c90
Rudra Jikadra <rhjikadra@gmail.com>
2018-06-17 23:44:22 +0530

    Initial Commit

--- Simple Video Player/ViewController.swift
+++ Simple Video Player/ViewController.swift
@@ -7,19 +7,144 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var play: UIButton!
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeSlider: UISlider!
+    
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    var url:URL!
+    
+    var isPlaying: Bool = false
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        url = URL(string : "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")
+        player = AVPlayer(url: url!)
+        player.currentItem?.addObserver(self, forKeyPath: "duration", options: [.new, .initial], context: nil)
+        addTimeObserver()
+        
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resize
+        
+        videoView.layer.addSublayer(playerLayer)
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        playerLayer.frame = videoView.bounds
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        if isPlaying {
+            player.pause()
+            play.setTitle("◉", for: .normal)
+            isPlaying = false
+            
+        } else {
+            player.play()
+            play.setTitle("◎", for: .normal)
+            isPlaying = true
+        }
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        let activity = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        activity.popoverPresentationController?.sourceView = self.view
+        
+        self.present(activity, animated: true, completion: nil)
+    }
+    
+    @IBAction func fastForward(_ sender: Any) {
+        
+        guard let duration = player.currentItem?.duration else {
+            return
+        }
 
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        let newTime = currentTime + 5.0
+
+        if newTime < (CMTimeGetSeconds(duration) - 5.0) {
+            let time: CMTime = CMTimeMake(Int64(newTime * 1000), 1000)
+            player.seek(to: time)
+        }
+        
+    }
+    
+    
+    @IBAction func goBackwards(_ sender: Any) {
+        
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        var newTime = currentTime - 5.0
+        
+        if newTime < 0 {
+            newTime = 0
+        }
+        let time: CMTime = CMTimeMake(Int64(newTime * 1000), 1000)
+        player.seek(to: time)
+    }
+    
+    @IBAction func timeSliderChanged(_ sender: UISlider) {
+        player.seek(to: CMTimeMake(Int64(sender.value*1000), 1000))
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "duration", let duration = player.currentItem?.duration.seconds, duration > 0.0 {
+            self.timeLabel.text = getTimeString(from: (player.currentItem?.duration)!)
+        }
+    }
+    
+    func addTimeObserver(){
+        let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        let mainQueue = DispatchQueue.main
+        
+        _ = player.addPeriodicTimeObserver(forInterval: interval, queue: mainQueue, using: { [weak self]time in
+            guard let currentItem = self?.player.currentItem else {return}
+            self?.timeSlider.maximumValue = Float(currentItem.duration.seconds)
+            self?.timeSlider.minimumValue = 0
+            self?.timeSlider.value = Float(currentItem.currentTime().seconds)
+            self?.currentTimeLabel.text = self?.getTimeString(from: currentItem.currentTime())
+        })
+    }
+    
+    func getTimeString(from time: CMTime) -> String {
+        let totalSeconds = CMTimeGetSeconds(time)
+        let hours = Int(totalSeconds/3600)
+        let minutes = Int(totalSeconds/60) % 60
+        let seconds = Int(totalSeconds.truncatingRemainder(dividingBy: 60))
+        if hours > 0 {
+            return String(format: "%i:%02i:%02i", arguments: [hours,minutes,seconds])
+        } else {
+            return String(format: "%02i:%02i", arguments: [minutes,seconds])
+        }
+        }
+        
+    }
+    
 
-
-}
 
